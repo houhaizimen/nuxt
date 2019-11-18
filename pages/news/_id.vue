0cde8b184fc3b25bc3e17c8a8aa170d5956b1490
cf <1679997020@qq.com>
2019-11-18 09:49:39 +0800

    'all'

---
+++ pages/news/_id.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>
+		<h2>News-content</h2>
+		<p>NewsId：{{$route.params.id}}</p>
+		<ul>
+			<li><a href="/">Home</a></li>
+		</ul>
+	</div>
+</template>
--- pages/news/index.vue
+++ pages/news/index.vue
@@ -2,9 +2,14 @@
   <div>
     <div>
 			<h2>news index vue</h2>
-			<p>{{$rout.params.newsId}}</p>
+			<p>{{$route.params.newsId}}</p>
 			<ul>
-				<li><nuxt-link :to="{name: 'index'}">HOME</nuxt-link></li>
+				<li>
+				<nuxt-link :to="{name: '/'}">Home</nuxt-link>
+			</li>
+			<li><a href="/news/123">news-1</a></li>
+			<li><nuxt-link to='/news/link'>news-link<news-link></news-link></nuxt-link></li>
+			<li><a href="/news/456">news-3</a></li>
 			</ul>
     </div>
   </div>
