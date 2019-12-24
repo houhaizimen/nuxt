bf90eb1d9ec1dedcce8bbd162f1e61197d43eb1b
cf <1679997020@qq.com>
2019-12-24 17:59:54 +0800

    'add'

--- user.js
+++ user.js
@@ -5,13 +5,30 @@ const query = require('./pool')
 
 user
 .get('/ageCount', async (ctx, next) => {
-    let sql  = 'select count(*) as total from user where age between ? and ? and sex = ?'
-    let data = await query(sql, [22, 24])
+    /**
+     * start 起始年龄  必传
+     * end 结束年龄  必传
+     */
+    let $sex = ctx.request.query.sex
+    let $start = ctx.request.query.start
+    let $end = ctx.request.query.end
+    let reqArr = []
+    let sql = ''
+    if (!$sex) {
+        sql = 'select count(*) as total from user where age between ? and ?'
+        reqArr = [$start, $end]
+    } else {
+        sql  = 'select count(*) as total from user where age between ? and ? and sex = ?'
+        reqArr = [$start, $end, $sex]
+    }
     ctx.body = data
 })
 .get('/addUser', async(ctx, next) => {
+    let $name = ctx.request.query.uname
+    let $sex = ctx.request.query.sex
+    let $age = ctx.request.query.age
     let sql = 'insert into user values(null,?, ?, ?)'
-    let data = await query(sql, [])
+    let data = await query(sql, [$name, $age, $sex])
     ctx.body = data
 })
 
